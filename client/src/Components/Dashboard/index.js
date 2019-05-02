78d169bda538a8a2a1d65075cee3fb23e0291150
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2019-05-02 03:31:25 -0400

    CRA with a few component skeletons set up

---
+++ client/src/Components/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import 'bulma/css/bulma.css'
+
+function Dashboard() {
+    return(
+        <h1>Dashboard!</h1>
+    )
+}
+
+export default Dashboard
